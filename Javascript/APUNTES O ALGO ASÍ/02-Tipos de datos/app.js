932f9b82d92837ea05ebe70fb0048dea53de7a0c
alvaroLZ96 <alvarost54@gmail.com>
2022-09-27 17:31:23 +0200

    feat: iteración 2

--- Javascript/APUNTES O ALGO ASÍ/02-Tipos de datos/app.js
+++ Javascript/APUNTES O ALGO ASÍ/02-Tipos de datos/app.js
@@ -1,2 +1,3 @@
 let nameschool = "neoland" +25 + 45; //saca neoland2545
 let hola = 25 + 45 + "neoland"// saca 70neoland
+//---------//
--- Javascript/basic_algorithms/app.js
+++
@@ -1,9 +0,0 @@
-c; /* reamos el repositorio
-        clonamos el repositorio y hacemos las carpetas necesarias
-git add .
-git commit -m "el comentario" 
-git push 
- */
-alt + shift + a;
-
-switch(superhero)
