15e042e047f2e058f4cceb47f44397525d7b6c81
alvaroLZ96 <alvarost54@gmail.com>
2022-10-01 12:10:58 +0200

    feat:iteración 7

--- Javascript/Functions/app.js
+++ Javascript/Functions/app.js
@@ -1,4 +1,4 @@
-/* // iteración1
+// iteración1
 function sum(numberOne , numberTwo) {
    return numberOne > numberTwo ? numberOne : numberTwo
   
@@ -20,23 +20,50 @@ const result = findLongestWord (avengers);
 console.log(result);
 
 //iteración3
-
+/* Calcular una suma puede ser tan simple como iterar sobre un array y sumar cada uno de los elementos.
+Implemente la función denominada sumNumbers que toma un array de números como argumento y devuelve la suma de todos los números de la matriz. */
 const numbers = [1, 2, 3, 5, 45, 37, 58];
+const sumNumbers= (numbers) => {
+    let total = 0;
+        for (let number of numbers){ 
+        total += number;
+       }
+    return total;
+    }
+console.log(sumNumbers(numbers)); 
 
-function sumAll() {
-for (number of numbers){
-    let total = numb
-}
+ //iteración 4
+ /* Calcular un promedio es una tarea extremadamente común. Puedes usar este array para probar tu función: */
+const numbers2 = [12, 21, 38, 5, 45, 37, 6]; 
+const average = (numbers2) =>  {
+    let total = 0;
+    for(let eachNumber of numbers2){
+        total+= eachNumber;
     
 }
-***************************************
-//iteración 4
+return total / numbers2.length;
+}
+console.log (average(numbers2));
 
 
-//iteración 5
+ //iteración 5
+/*   Crea una función que reciba por parámetro un array y cuando es un valor number lo sume 
+ y de lo contrario cuente la longitud del string y lo sume. Puedes usar este array para probar tu función:  */
+const mixedElements = [6, 1, 'Rayo', 1, 'vallecano', '10', 'upgrade', 8, 'hub'];
+const averageWord = (param) => {
+    let finalSum = 0
+    for(let element of mixedElements){
+        if (typeof element == "string"){
+       finalSum += element.length;
+        } else if (typeof element  == "number"){
+           finalSum += element;
+        }
+    }
+return finalSum;
+}
+console.log(averageWord(mixedElements));
 
-
-// iteración 6  */
+// iteración 6
 
 const duplicates = [
     'sushi',
@@ -61,3 +88,55 @@ let arrayNoRepeat =[];
 }
 
 console.log(removeDuplicates(duplicates))
+
+//iteración 7
+/* Crea una función que reciba por parámetro un array y el valor que desea comprobar que existe dentro de dicho array 
+- comprueba si existe el elemento, en caso que existan nos devuelve un true y la posición de dicho elemento y por la contra un false.
+ */
+const nameFinder = [
+    'Peter',
+    'Steve',
+    'Tony',
+    'Natasha',
+    'Clint',
+    'Logan',
+    'Xabier',
+    'Bruce',
+    'Peggy',
+    'Jessica',
+    'Marc'
+  ];
+
+const finderName = (array, param2) => {
+
+    for (let name of array){
+    if (array.includes(param2)) {
+console.log(array.indexOf(name))
+return true
+    } else {
+return false;
+ }
+}
+ }
+console.log (finderName(nameFinder, "Peter"))
+
+//Iteration #8: Contador de repeticiones
+/* Crea una función que nos devuelva el número de veces
+que se repite cada una de las palabras que lo conforma.
+Puedes usar este array para probar tu función: */
+   const counterWords = [
+    'code',
+    'repeat',
+    'eat',
+    'sleep',
+    'code',
+    'enjoy',
+    'sleep',
+    'code',
+    'enjoy',
+    'upgrade',
+    'code'
+  ];
+  function repeatCounter(param) {
+    // insert code
+  }
